79626aa125a968a83e4c8793de417c6d39a6c2ce
STARS <adelabdelhamed11@gmail.com>
2017-05-23 23:39:14 +0200

    Initial commit

---
+++ example-arrow-functions.js
@@ -0,0 +1,41 @@
+// var names = ['Anrew', 'Julie', 'Joe'];
+//
+// names.forEach(function(name){
+//   console.log('forEach', name);
+// });
+//
+// names.forEach((name) =>{
+//   console.log('ArrowFunc', name);
+// });
+//
+// names.forEach((name) => console.log(name));
+
+
+// var returnMe = (name) => name + '!';
+// console.log(returnMe ('andrew'));
+
+// var person = {
+//   name : 'Andrew',
+//   greet: function () {
+//   names.forEach((name) => {
+// console.log(this.name  + ' says hi to ' +  name)
+//   });
+//   }
+// };
+//
+// person.greet();
+
+//challenge area
+function add(a, b){
+  return a + b;
+}
+//addStatement
+var addStatement = (a , b) => {
+  return a + b;
+}
+console.log(addStatement(4 , 7));
+
+//addExpression
+
+var addExpression = (a, b) => a+ b;
+console.log(addExpression(3 , 3));
